12847b7dacb61f26dce080ccb787f58231e67c64
kaka1409 <khanhphqgcs230461@fpt.edu.vn>
2026-01-19 15:28:32 +0700

    add: public api (for mobile maybe)

---
+++ src/app/api/auth/register/route.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from 'next/server';
+import { axiosBackend } from '@/lib/axios';
+
+export async function POST(request: Request) {
+    try {
+        const body = await request.json();
+        // console.log("Register request (server)", body);
+
+        // const response = await axiosBackend.post('/auth/register', body);
+        // return NextResponse.json(response.data);
+
+        return NextResponse.json({
+            status: 200,
+            message: "Register successful",
+            data: {
+                accessToken: "",
+            }
+        }); 
+    } catch (error: any) {
+        console.error('Error registering:', error.response?.data?.message || error.message);
+        return NextResponse.json(
+            { message: error.response?.data?.message || error.message },
+            { status: error.response?.status || 500 }
+        );
+    }
+}
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,16 +1,10 @@
+'use server';
+
 import { axiosFrontend } from '@/lib/axios';
 import { AxiosResponse } from 'axios';
 
-// Example generic fetcher using the Frontend Instance
-export const fetcher = async <T>(url: string): Promise<T> => {
-    const response: AxiosResponse<T> = await axiosFrontend.get(url);
-    return response.data;
-};
+// use for public api 
 
-// Example service object interacting with Next.js API Routes
-export const authService = {
-    login: async (credentials: any) => {
-        // This calls /api/auth/login (Next.js) which then proxies to Java
-        return axiosFrontend.post('/auth/login', credentials);
-    },
-};
+// export const productService = {
+    
+// }
